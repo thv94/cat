ffa185a57dd9dc5c82aa9b07b16ad9bd82c24121
Taylor Vaughn <78456999+thv94@users.noreply.github.com>
2025-08-11 22:30:10 -0400

    Added line numbers

--- makefile
+++ makefile
@@ -1,24 +1,44 @@
 CC = gcc
 CSTD = c90
-OPT_LEVEL = -O2
+
+DEBUG ?= 0
 
 SRC_DIR = src
 INC_DIR = include
 OBJ_DIR = .o
+DEBUG_BUILD_DIR = builds/debug
+RELEASE_BUILD_DIR = builds/release
 
-CFLAGS = -Wall -Wextra -Werror -Wpedantic -I$(INC_DIR) -std=$(CSTD) $(OPT_LEVEL) -Wshadow -Wpointer-arith -Wcast-align -Wstrict-prototypes -Wcast-qual -Wswitch-default -Wswitch-enum -Wunreachable-code -Wfloat-equal -Wconversion -Wdouble-promotion -Wmissing-prototypes -Wundef -Wunused-macros -Wwrite-strings
+CFLAGS = -Wall -Wextra -Werror -Wpedantic -I$(INC_DIR) -std=$(CSTD) -Wshadow -Wpointer-arith -Wcast-align -Wstrict-prototypes -Wcast-qual -Wswitch-default -Wswitch-enum -Wunreachable-code -Wfloat-equal -Wconversion -Wdouble-promotion -Wmissing-prototypes -Wundef -Wunused-macros -Wwrite-strings
 
 TARGET = mycat
+
+ifeq ($(DEBUG), 1)
+	BUILD_DIR = $(DEBUG_BUILD_DIR)
+	CFLAGS += -g -O0
+else
+	BUILD_DIR = $(RELEASE_BUILD_DIR)
+	CFLAGS += -O2
+endif
 
 SRC_FILES = $(wildcard $(SRC_DIR)/*.c)
 OBJ_FILES = $(patsubst $(SRC_DIR)/%.c,$(OBJ_DIR)/%.o,$(SRC_FILES))
 
-all: $(OBJ_DIR) $(TARGET)
+all: $(OBJ_DIR) $(BUILD_DIR)/$(TARGET)
+
+debug:
+	$(MAKE) all DEBUG=1
+
+release:
+	$(MAKE) all DEBUG=0
 
 $(OBJ_DIR):
 	mkdir -p $(OBJ_DIR)
 
-$(TARGET): $(OBJ_FILES)
+$(BUILD_DIR):
+	mkdir -p $(BUILD_DIR)
+
+$(BUILD_DIR)/$(TARGET): $(OBJ_FILES)
 	@mkdir -p $(@D)
 	$(CC) $(CFLAGS) $^ -o $@
 
@@ -26,4 +46,4 @@ $(OBJ_DIR)/%.o: $(SRC_DIR)/%.c | $(OBJ_DIR)
 	$(CC) $(CFLAGS) -c $< -o $@
 
 clean:
-	rm -rf $(OBJ_FILES) $(TARGET)
+	rm -rf $(OBJ_FILES) $(DEBUG_BUILD_DIR)/$(TARGET) $(RELEASE_BUILD_DIR)/$(TARGET)
--- src/mycat.c
+++ src/mycat.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "utils.h"
 
 #define KB        1024
 #define NUM_KB    8
 #define BUFSIZE   (NUM_KB * KB)
+#define LINE_NUM_BUFF_SIZE 20
 
 void stdin_cat(void);
 void simple_cat(const char* filename);
@@ -42,7 +44,10 @@ void simple_cat(const char* filename)
 {
     char buffer[BUFSIZE];
     size_t bytes_read;
+    size_t i;
     FILE *input_file;
+    char line_num_str[LINE_NUM_BUFF_SIZE];
+    int line_number = 1;
 
     if (is_directory(filename))
     {
@@ -58,10 +63,30 @@ void simple_cat(const char* filename)
         perror(NULL);
         return;
     }
+   
+    /* This assumes we have a first line, probably need a check here
+       that the file is not empty */
+    sprintf(line_num_str, "%6d   ", line_number);
+    fwrite(line_num_str, 1, strlen(line_num_str), stdout);
+    line_number++;
         
     while ((bytes_read = fread(buffer, sizeof(buffer[0]), sizeof(buffer), input_file)) > 0)
     {
-        fwrite(buffer, sizeof(buffer[0]), bytes_read, stdout);
+        for (i = 0; i < bytes_read; i++)
+        {
+            /* End of buffer will be 0's not EOF like reading a file */
+            if ((buffer[i] == '\n') && (buffer[i+1] != 0))
+            {
+                fwrite(&buffer[i], 1, 1, stdout);
+                sprintf(line_num_str, "%6d   ", line_number);
+                fwrite(line_num_str, 1, strlen(line_num_str), stdout);
+                line_number++;
+            }
+            else
+            {
+                fwrite(&buffer[i], 1, 1, stdout);
+            }
+        }
     }
 
     fclose(input_file);
