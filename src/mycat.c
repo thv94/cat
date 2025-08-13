cf444a40da2754592e72866c574e3c6ed27bfd7c
Taylor Vaughn <78456999+thv94@users.noreply.github.com>
2025-08-12 22:40:53 -0400

    Added flag for line numbers

--- src/mycat.c
+++ src/mycat.c
@@ -1,65 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <getopt.h>
 #include "utils.h"
+
+typedef enum
+{
+    FALSE,
+    TRUE
+} BOOL_T;
 
 #define KB        1024
 #define NUM_KB    8
 #define BUFSIZE   (NUM_KB * KB)
 #define LINE_NUM_BUFF_SIZE 20
-#define STARTING_LINE_NUMBER 1
+#define DEFAULT_STARTING_LINE_NUMBER 1
+#define LINE_NUMBER_FORMAT_STR "%6d   "
 
-void stdin_cat(void);
-int simple_cat(const char* filename, int line_number);
+void stdin_cat(BOOL_T enable_line_numbers);
+void simple_cat(const char* filename, BOOL_T enable_line_number);
 void multi_cat(int argc, char **argv);
 
 int main(int argc, char **argv)
 {
-    switch(argc)
+    int opt;
+    BOOL_T enable_line_numbers = FALSE;
+
+    while ((opt = getopt(argc, argv, "n")) != -1)
     {
-        case 1:
-            stdin_cat();
-            break;
-        case 2:
-            simple_cat(argv[1], STARTING_LINE_NUMBER);
-            break;
-        default:
-            multi_cat(argc, argv);
-            break;
+        switch(opt)
+        {
+            case 'n':
+                enable_line_numbers = TRUE;
+                break;
+            default:
+            case '?':
+                fprintf(stderr, "Unknown option: -%c\n", optopt);
+                return EXIT_FAILURE;
+        }
+    }
+
+    if (optind == argc)
+    {
+        stdin_cat(enable_line_numbers);
+    }
+    else
+    {
+        /* Process remaining file names */
+        for (; optind < argc; optind++)
+        {
+            simple_cat(argv[optind], enable_line_numbers);
+        }
     }
 
     return EXIT_SUCCESS;
 }
 
-void stdin_cat(void)
+void stdin_cat(BOOL_T enable_line_numbers)
 {
     char buffer[BUFSIZE];
     char line_num_str[LINE_NUM_BUFF_SIZE];
-    int line_number = STARTING_LINE_NUMBER;
+    int line_number = DEFAULT_STARTING_LINE_NUMBER;
 
     while (NULL != fgets(buffer, sizeof(buffer), stdin))
     {
-        sprintf(line_num_str, "%6d   ", line_number);
-        line_number++;
-        fputs(line_num_str, stdout);
+        if(enable_line_numbers)
+        {
+            sprintf(line_num_str, LINE_NUMBER_FORMAT_STR, line_number);
+            line_number++;
+            fputs(line_num_str, stdout);
+        }
+
         fputs(buffer, stdout);
     }
 }
 
-int simple_cat(const char* filename, int start_line_index)
+void simple_cat(const char* filename, BOOL_T enable_line_numbers)
 {
     char buffer[BUFSIZE];
     size_t bytes_read;
-    size_t i;
+    size_t read_buffer_idx;
     FILE *input_file;
     char line_num_str[LINE_NUM_BUFF_SIZE];
-    int line_number = start_line_index;
+    static int line_number = DEFAULT_STARTING_LINE_NUMBER;
     long file_size;
 
     if (is_directory(filename))
     {
         fprintf(stderr, "%s is a directory.\n", filename);
-        return STARTING_LINE_NUMBER;
     }
     
     input_file = fopen(filename, "r");
@@ -68,7 +96,6 @@ int simple_cat(const char* filename, int start_line_index)
     {
         fprintf(stderr, "Error opening file %s: ", filename);
         perror(NULL);
-        return STARTING_LINE_NUMBER;
     }
 
     /* Check for empty file */
@@ -78,42 +105,39 @@ int simple_cat(const char* filename, int start_line_index)
    
     if (file_size > 0)
     {
-        sprintf(line_num_str, "%6d   ", line_number);
-        fwrite(line_num_str, 1, strlen(line_num_str), stdout);
-        line_number++;
+        if (enable_line_numbers)
+        {
+            sprintf(line_num_str, LINE_NUMBER_FORMAT_STR, line_number);
+            fwrite(line_num_str, 1, strlen(line_num_str), stdout);
+            line_number++;
+        }
             
         while ((bytes_read = fread(buffer, sizeof(buffer[0]), sizeof(buffer), input_file)) > 0)
         {
-            for (i = 0; i < bytes_read; i++)
+            for (read_buffer_idx = 0; read_buffer_idx< bytes_read; read_buffer_idx++)
             {
-                /* End of buffer will be 0's not EOF like reading a file (WILL IT?)*/
-                if ((buffer[i] == '\n') && (buffer[i+1] != 0))
+                if (enable_line_numbers)
                 {
-                    fwrite(&buffer[i], 1, 1, stdout);
-                    sprintf(line_num_str, "%6d   ", line_number);
-                    fwrite(line_num_str, 1, strlen(line_num_str), stdout);
-                    line_number++;
+                    /* End of buffer will be 0's not EOF like reading a file */
+                    if ((buffer[read_buffer_idx] == '\n') && (buffer[read_buffer_idx+1] != 0))
+                    {
+                        fwrite(&buffer[read_buffer_idx], 1, 1, stdout);
+                        sprintf(line_num_str, LINE_NUMBER_FORMAT_STR, line_number);
+                        fwrite(line_num_str, 1, strlen(line_num_str), stdout);
+                        line_number++;
+                    }
+                    else
+                    {
+                        fwrite(&buffer[read_buffer_idx], 1, 1, stdout);
+                    }
                 }
                 else
                 {
-                    fwrite(&buffer[i], 1, 1, stdout);
+                    fwrite(&buffer[read_buffer_idx], 1, 1, stdout);
                 }
             }
         }
     }
 
     fclose(input_file);
-
-    return line_number;
-}
-
-void multi_cat(int argc, char **argv)
-{
-    int i;
-    int line_number = STARTING_LINE_NUMBER;
-
-    for (i = 1; i < argc; i++)
-    {
-        line_number = simple_cat(argv[i], line_number);
-    }
 }
